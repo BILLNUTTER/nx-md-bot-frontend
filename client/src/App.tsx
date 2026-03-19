d64bf6365afc65a9cce8114f5e824068fdda3509
nutterx technologies <omayiocalvin59@gmail.com>
2026-03-19 13:55:11 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Router as WouterRouter, Switch, Route } from "wouter";
+import { Routes, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -12,16 +12,14 @@ import AdminPage from "@/pages/admin";
 
 function AppRouter() {
   return (
-    <WouterRouter>
-      <Switch>
-        <Route path="/" component={LandingPage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/register" component={RegisterPage} />
-        <Route path="/dashboard" component={DashboardPage} />
-        <Route path="/admin" component={AdminPage} />
-        <Route component={NotFound} />
-      </Switch>
-    </WouterRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/admin" element={<AdminPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
